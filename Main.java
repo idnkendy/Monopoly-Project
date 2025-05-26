b94a530da335c8795f8188894eda6424c4662ecc
idnkendy <idnkendy@gmail.com>
2025-05-27 01:07:35 +0700

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.SwingUtilities;
-import ui.MonopolyGame;
-
-public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new MonopolyGame());
-    }
-}
